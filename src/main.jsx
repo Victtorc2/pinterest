b37fbfd8eb1226ccd53e15b46ee069df3639cab4
Victtorc2 <viccx20004@hotmail.com>
2025-07-26 23:57:23 -0500

    hackaton16

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
